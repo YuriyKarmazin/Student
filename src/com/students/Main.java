1875b7caa8e04d44b8b8b607985315cc514bc433
YuriyKarmazin <YuriyKarmazin>
2021-03-01 15:21:58 +0200

    Practice-2

--- src/com/students/Main.java
+++ src/com/students/Main.java
@@ -1,5 +1,7 @@
 package com.students;
 
+import static com.students.Student.countStudent;
+
 public class Main {
     public static void main(String[] args) {
         Student Peter = new Student("McCauley", 1234);
@@ -7,6 +9,11 @@ public class Main {
         Student Mark = new Student(1234);
         Student Karl = new Student();
 
-        // write your code here
+        Peter.printStudents();
+        Jack.printStudents();
+        Mark.printStudents();
+        Karl.printStudents();
+
+        System.out.println("Number of students is: " + countStudent);
     }
 }
--- src/com/students/Student.java
+++ src/com/students/Student.java
@@ -5,25 +5,29 @@ public class Student {
     public String lastName;
     public int studentID;
 
-    public Student (String lastName, int studentID){
+    public Student(String lastName, int studentID) {
         this.lastName = lastName;
         this.studentID = studentID;
         countStudent++;
-        System.out.println("The first student is " + lastName + " and his ID is: " + studentID);
     }
-    public Student (String lastName){
+
+    public Student(String lastName) {
         this.lastName = lastName;
         countStudent++;
-        System.out.println("The second student is " + lastName);
     }
-    public Student (int studentID){
+
+    public Student(int studentID) {
         this.studentID = studentID;
         countStudent++;
-        System.out.println("The third student is ... and his ID is: " + studentID);
     }
-    public Student (){
+
+    public Student() {
         countStudent++;
-        System.out.println("The fourth student is ... ");
-        System.out.println(" Number of students is: " + countStudent);
+    }
+
+    void printStudents (){
+        System.out.println("last Name: " + lastName);
+        System.out.println("ID: " + studentID);
+        System.out.println(" ");
     }
 }
